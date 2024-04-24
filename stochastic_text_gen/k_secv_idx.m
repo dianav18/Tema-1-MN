822b974545479d578db9fdb6f887102c7f859404
Vulpoi Maria-Diana <dianavulpoi3@gmail.com>
2024-04-25 00:54:29 +0300

    100 de puncte, dar neslefuita

--- stochastic_text_gen/k_secv_idx.m
+++ stochastic_text_gen/k_secv_idx.m
@@ -1,10 +1,5 @@
-function retval = k_secv_idx(distinct_k_sec)
-    % Creăm un container Map gol
-    retval = containers.Map('KeyType', 'char', 'ValueType', 'int32');
-    
-    % Parcurgem fiecare secvență din array-ul de secvențe k distincte
-    for i = 1:length(distinct_k_sec)
-        sequence = distinct_k_sec{i};  % Extragem fiecare secvență
-        retval(sequence) = i;  % Asociem secvența cu indicele său
-    end
-end
+function retval = k_secv_idx (distinct_k_sec)
+	
+	retval = containers.Map(distinct_k_sec, 1:length(distinct_k_sec));
+
+endfunction
--- stochastic_text_gen/word_idx.m
+++ stochastic_text_gen/word_idx.m
@@ -1,10 +1,5 @@
-function retval = word_idx(distinct_wds)
-    % Creăm un container Map gol
-    retval = containers.Map('KeyType', 'char', 'ValueType', 'int32');
-    
-    % Parcurgem fiecare cuvânt din array-ul de cuvinte distincte
-    for i = 1:length(distinct_wds)
-        word = distinct_wds{i};  % Extragem fiecare cuvânt
-        retval(word) = i;  % Asociem cuvântul cu indicele său
-    end
-end
+function retval = word_idx (distinct_wds)
+	
+	retval = containers.Map(distinct_wds, 1:length(distinct_wds));
+
+endfunction
